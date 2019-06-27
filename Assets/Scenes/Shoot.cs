22088258a939900c6c5cac048d2d06d238dabe3a
Naoto Yoshioka <yos@pb3.so-net.ne.jp>
2019-06-27 13:18:08 +0900

    WIP

--- Assets/Scenes/Shoot.cs
+++ Assets/Scenes/Shoot.cs
@@ -22,7 +22,12 @@ public class Shoot : MonoBehaviour
     {
         var scale = startScale;
         var pos = transform.position + spawnDistrance*transform.forward;
-        var obj = Instantiate(prefab, pos, Quaternion.identity, parent);
+        // var obj = Instantiate(prefab, pos, Quaternion.identity, parent);
+        var obj = Instantiate(prefab, parent);
+        // obj.transform.SetPositionAndRotation(pos, Quaternion.identity);
+        // obj.transform.SetPositionAndRotation(transform.position, Quaternion.identity);
+        obj.transform.position = transform.position;
+
         while (!OVRInput.GetUp(button, controller))
         {
             obj.transform.localScale = scale*Vector3.one;
